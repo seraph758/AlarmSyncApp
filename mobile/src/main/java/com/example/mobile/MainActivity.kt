0efc67005c8e6fe820a96b0ba99040b07847b4f5
seraph758 <luke.likai@gmail.com>
2025-08-22 03:56:58 +0800

    Add files via upload

---
+++ mobile/src/main/java/com/example/mobile/MainActivity.kt
@@ -0,0 +1,12 @@
+
+package com.example.mobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(android.R.layout.simple_list_item_1)
+    }
+}
